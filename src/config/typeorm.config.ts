1e316acf3db0f07173d7605b357a4591ebd26b97
zeljkoantich <zeljko.antich@gmail.com>
2019-07-20 15:17:56 +0200

    typeorm.config: EntityMetadataNotFound error fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,9 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 // back form current directory, any folder, any file that ends with '.entity.ts'
-const entitiesLocation = `${__dirname}/../**/*.entity.ts`;
+// const entitiesLocation = `${__dirname}/../**/*.entity.ts`; // if .ts error: EntityMetadataNotFound: No metadata for "TaskEntity" was found.
+
+const entitiesLocation = `${__dirname}/../**/*.entity.js`;
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "postgres",
