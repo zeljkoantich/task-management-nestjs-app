df44764bbdf489997c912b85a3be8c59738b0d58
zeljkoantich <zeljko.antich@gmail.com>
2019-07-20 11:31:55 +0200

    change NotAcceptableException to BadRequestException

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,4 +1,4 @@
-import { PipeTransform, NotAcceptableException } from "@nestjs/common";
+import { PipeTransform, BadRequestException } from "@nestjs/common";
 import { TaskStatuses } from "../models/task.model";
 
 export class TaskStatusValidationPipe implements PipeTransform {
@@ -8,7 +8,7 @@ export class TaskStatusValidationPipe implements PipeTransform {
     const isTaskStatusValueValid = taskStatuses.some(status => status === value);
 
     if (!isTaskStatusValueValid) {
-      throw new NotAcceptableException(`Status ${value} is not a valid status`);
+      throw new BadRequestException(`Status ${value} is not a valid status`);
     }
 
     return value;
