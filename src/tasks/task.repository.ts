53e1cd0d469a47a2ee36836b28184b78fa827664
zeljkoantich <zeljko.antich@gmail.com>
2019-07-20 14:37:16 +0200

    add task.repository

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from "typeorm";
+import { TaskEntity } from "./task.entity";
+
+@EntityRepository(TaskEntity)
+export class TaskRepository extends Repository<TaskEntity> {
+
+}
