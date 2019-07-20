8603cb394822936724984c6743a3f828bfa5bd40
zeljkoantich <zeljko.antich@gmail.com>
2019-07-20 11:25:47 +0200

    task-status-validation-pipe: const rename
    remove unused ArgumentMetadata

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,13 +1,13 @@
-import { PipeTransform, ArgumentMetadata, NotAcceptableException } from "@nestjs/common";
+import { PipeTransform, NotAcceptableException } from "@nestjs/common";
 import { TaskStatuses } from "../models/task.model";
 
 export class TaskStatusValidationPipe implements PipeTransform {
 
-  transform(value: TaskStatuses, metadata: ArgumentMetadata) {
+  transform(value: TaskStatuses) {
     const taskStatuses = Object.values(TaskStatuses);
-    const isTaskValueValid = taskStatuses.some(status => status === value);
+    const isTaskStatusValueValid = taskStatuses.some(status => status === value);
 
-    if (!isTaskValueValid) {
+    if (!isTaskStatusValueValid) {
       throw new NotAcceptableException(`Status ${value} is not a valid status`);
     }
 
