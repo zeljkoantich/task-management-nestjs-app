d452ab65896c055e1b10a397695bddb89bc3c2be
zeljkoantich <zeljko.antich@gmail.com>
2019-07-24 16:28:45 +0200

    dev & debug settings in typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,8 +1,14 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-// back form current directory, any folder, any file that ends with '.entity.ts'
+/**
+ * USE THIS WHEN DEBUGGING: npm run start:debug
+ */
+// (back form current directory, any folder, any file that ends with '.entity.ts')
 // const entitiesLocation = `${__dirname}/../**/*.entity.ts`; // if .ts error: EntityMetadataNotFound: No metadata for "TaskEntity" was found.
 
+/**
+ * USE THIS WHEN DEVELOPING: npm run start:dev
+ */
 const entitiesLocation = `${__dirname}/../**/*.entity.js`;
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
